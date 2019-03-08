9a807c11d48e58c66802860c316ae299394a0dc1
jojordan3 <joanne.k.m.jordan@gmail.com>
2019-03-07 20:34:58 -0800

    boilerplate for twitter app

---
+++ flask_miniapp/__init__.py
@@ -0,0 +1,6 @@
+'''
+Entry point for mini app
+'''
+from .app import create_app
+
+APP = create_app()
---
+++ flask_miniapp/models.py
@@ -0,0 +1,49 @@
+'''
+SQLalchemy models for the mini application
+'''
+from flask_sqlalchemy import SQLAlchemy
+
+DB = SQLAlchemy()
+
+
+class User(DB.Model):
+    '''Twitter users that we pull and analyze tweets for
+    '''
+    id = DB.Column(DB.Integer, primary_key=True)
+    name = DB.Column(DB.String(15), nullable=False)
+
+    def __repr__(self):
+        return '<User {}>'.format(self.name)
+
+
+class Tweet(DB.Model):
+    '''Tweets
+    '''
+    id = DB.Column(DB.Integer, primary_key=True)
+    text = DB.Column(DB.Unicode(320))
+    user_id = DB.Column(DB.Integer, DB.ForeignKey('user.id'), nullable=False)
+    user = DB.relationship('User', backref=DB.backref('tweets'), lazy=True)
+
+    def __repr__(self):
+        return '<Tweet: {}>'.format(self.text)
+
+
+'''
+>>> from flask_miniapp.models import *
+>>> u1 = User(name='austen')
+>>> t1 = Tweet(text='Lambda rocks')
+>>> u1.tweets
+[]
+>>> u1.tweets.append(t1)
+>>> u1
+<User austen>
+>>> u1.tweets
+[<Tweet: Lambda rocks>]
+>>> DB.session.add(u1)
+>>> DB.session.add(t1)
+>>> DB.session.commit()
+>>> User.query.filter(User.name=='austen').first()
+<User austen>
+>>> User.query.filter(User.name=='austen').first().tweets
+[<Tweet: Lambda rocks>]
+'''
